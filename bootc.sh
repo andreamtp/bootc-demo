243c5f79c375feecb10df5efe32265eaa2029a0e
Andrea Perotti <aperotti@redhat.com>
2024-10-22 10:15:44 +0200

    Repo restructured
    Moved from an ad hoc demo script to a parametric one (bootc.sh).
    Each target distro has its own directory and its .toml config files.
    Target artifacts are created in the respective distro directories.

--- bootc.sh
+++ bootc.sh
@@ -2,9 +2,39 @@
 
 source demo.sh/demo.sh
 
-CONTAINERFILE=f40.container
-TAG=f40-bootc:demo
-IMAGE_BUILDER_CONF=config-qcow2.toml
+# Check if two parameters are passed
+if [ "$#" -ne 2 ]; then
+  echo "Error: Two parameters are required."
+  echo "Usage: $0 [f40 | cs9] [qcow2 | iso | ks]"
+  exit 1
+fi
+
+# Validate the first parameter (f40 or cs9)
+if [[ "$1" != "f40" && "$1" != "cs9" ]]; then
+  echo "Error: The first parameter must be either 'f40' or 'cs9'."
+  exit 1
+fi
+
+# Validate the second parameter (qcow2, iso, or ks)
+if [[ "$2" != "qcow2" && "$2" != "iso" && "$2" != "ks" ]]; then
+  echo "Error: The second parameter must be 'qcow2', 'iso', or 'ks'."
+  exit 1
+fi
+
+DISTRO=$1
+TARGET=$2
+
+CONTAINERFILE=${DISTRO}/${DISTRO}.container
+TAG=${DISTRO}-bootc:linuxday
+IMAGE_BUILDER_CONF=${DISTRO}/config-${TARGET}.toml
+REGISTRY=quay.io/aperotti
+$REPO=${REGISTRY}/$TAG
+if [[ "$TARGET" == "qcow2" ]]; then
+    OUTPUTDIR=${DISTRO}/$TARGET
+else
+    OUTPUTDIR=${DISTRO}/bootiso
+fi
+
 
 ps1() {
 #    echo -ne "\$ \033[00m"
@@ -32,7 +62,7 @@ pei "podman build -f $CONTAINERFILE -t $TAG ."
 echo ""
 pause
 # we want to have this container avail also for root user
-podman image scp $(whoami)@localhost::$TAG
+# podman image scp $(whoami)@localhost::$TAG
 clear
 pei "bat -n $IMAGE_BUILDER_CONF"
 echo ""
@@ -48,10 +78,10 @@ echo "sudo podman run
 -v $(pwd)/$IMAGE_BUILDER_CONF:/config.toml:ro
 -v /var/lib/containers/storage:/var/lib/containers/storage
 quay.io/centos-bootc/bootc-image-builder:latest
---type qcow2
+--type $TARGET
 --rootfs xfs
 --local
-localhost/$TAG" | highlight -S sh -O ansi
+$REPO" | highlight -S sh -O ansi
 echo ""
 pause
 
@@ -65,12 +95,12 @@ sudo podman run \
     -v $(pwd)/$IMAGE_BUILDER_CONF:/config.toml:ro \
     -v /var/lib/containers/storage:/var/lib/containers/storage \
     quay.io/centos-bootc/bootc-image-builder:latest \
-    --type qcow2 \
+    --type $TARGET \
     --rootfs xfs \
     --local \
-    localhost/$TAG
+    $REPO
 
-pei 'ls -lsah output/qcow2/'
+pei "ls -lsah $OUTPUTDIR"
 
 # type the command automatically'
 # DEMO_AUTO_TYPE=yes
